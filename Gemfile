09c8d432308b10df8c558bbbbed69c033a154596
Alexander <alex.997.gape@hotmail.com>
2023-06-24 22:51:01 -0500

    CRUD finished

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
+
+gem "erb-formatter"
+
 ruby "3.2.2"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # @article = Article.new(title: "My Article", body: "This is my article.")
+    # @article = Article.new(params.require(:article).permit(:title, :body))
+    @article = Article.new(article_params)
+
+    if @article.save
+      # puts 'article saved'
+      # puts @article.inspect
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params) # if the article is updated successfully
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end
